2ce4dacc883b6d1ec28fa018406f5f1ef4877c26
Ibadullaev_Ali <ali.ibadullayev.02@gmail.com>
2025-06-20 18:31:58 +0300

    update CORS configuration; add new allowed origin for backend service

--- backend/src/main/java/org/theopen/backend/config/CorsConfig.java
+++ backend/src/main/java/org/theopen/backend/config/CorsConfig.java
@@ -13,7 +13,7 @@ public class CorsConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:5173", "http://localhost:3000", "https://theopenvpn.app")
+                        .allowedOrigins("http://localhost:5173", "http://localhost:3000", "https://theopenvpn.app", "https://theopen-backend.web.app/")
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                         .allowedHeaders("*")
                         .exposedHeaders("Location", "Content-Disposition")
